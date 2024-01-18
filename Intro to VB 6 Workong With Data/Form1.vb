9c23701c70ca50f69ac8a7fecd95c3b21ca99ac1
Lenovo-User <Lenovo-User@DESKTOP-4ACQTBU>
2024-01-18 20:10:40 +0200

    Add project files.

---
+++ Intro to VB 6 Workong With Data/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMessage As String
+
+        Dim Birthyear As Integer
+        YourMessage = "Happy birthday! How old are you now"
+
+        MsgBox(YourMessage)
+        Birthyear = Val(InputBox("Enter year Of birth"))
+        MsgBox("you are " & 2023 - Birthyear & " years old today")
+
+    End Sub
+End Class
---
+++ Intro to VB 6 Workong With Data/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Intro to VB 6 Workong With Data")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Intro to VB 6 Workong With Data")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3ab3deed-0a0e-4958-a804-a772f4a8e7a8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
